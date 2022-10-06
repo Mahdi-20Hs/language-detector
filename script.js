2a8beed380eaa7d62e709eb8fe4bfd68206b061e
Mahdi <mahdihs395@gmail.com>
2022-10-06 19:51:01 +0300

    finished building the interface

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const textarea = document.querySelector('#textarea');
+const btn = document.querySelector('button');
+const result = document.querySelector('.result');
+
+btn.addEventListener('click', () => {
+  console.log(franc('hello'))
+})
